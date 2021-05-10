d2586c34f7164db203b4ffd9ce3bf271f620f8c4
Leonardo Silva <silvaleonardo.ti@gmail.com>
2021-05-10 17:01:42 -0300

    Selection sort algorithm implementation

--- src/App.java
+++ src/App.java
@@ -3,16 +3,18 @@ import java.util.Arrays;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        // useBinarySearch();
-        // useInsertionSort();
+        int[] array = { 1, 12, 5, 6, 17, 2, 9, 11, 20, 14, 10 };
+
+        useBinarySearch(array);
+        useInsertionSort(array);
+        useSelectionSort(array);
 
     }
 
-    public static void useBinarySearch() {
-        int[] array = { 1, 12, 5, 6, 17, 2, 9, 11, 20, 14, 10 };
+    public static void useBinarySearch(int[] array) {
         Arrays.sort(array);
         System.out.println(Arrays.toString(array));
-        System.out.println("Result: " + binarySearch(array, 9, 0, array.length - 1));
+        System.out.println("Binary search result: " + binarySearch(array, 9, 0, array.length - 1));
     }
 
     public static int binarySearch(int[] array, int arg, int inf, int sup) {
@@ -33,10 +35,9 @@ public class App {
         return -1;
     }
 
-    public static void useInsertionSort() {
-        int[] array = { 1, 12, 5, 6, 17, 2, 9, 11, 20, 14, 10 };
+    public static void useInsertionSort(int[] array) {
         System.out.println(Arrays.toString(array));
-        System.out.println(Arrays.toString(insertionSort(array, array.length)));
+        System.out.println("Insertion sort result: " + Arrays.toString(insertionSort(array, array.length)));
     }
 
     public static int[] insertionSort(int[] array, int n) {
@@ -55,6 +56,34 @@ public class App {
         }
         array[j + 1] = last;
         return array;
+    }
+
+    public static void useSelectionSort(int[] array) {
+        System.out.println(Arrays.toString(array));
+        System.out.println("Selection sort result: " + Arrays.toString(selectionSort(array, array.length, 0)));
+    }
+
+    public static int[] selectionSort(int[] array, int n, int index) {
+        if (index == n)
+            return array;
+
+        int k = minIndex(array, index, n - 1);
+
+        if (k != index) {
+            int temp = array[k];
+            array[k] = array[index];
+            array[index] = temp;
+        }
+        return selectionSort(array, n, index + 1);
+    }
+
+    public static int minIndex(int a[], int i, int j) {
+        if (i == j)
+            return i;
+
+        int k = minIndex(a, i + 1, j);
+
+        return (a[i] < a[k]) ? i : k;
     }
 
 }
